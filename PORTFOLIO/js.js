9103f856bcaee68f166b1d6c9dca02e106b38a99
Matheus Henrique <matheus260524@gmail.com>
2026-09-24 07:52:07 -0300

    Add files via upload

---
+++ PORTFOLIO/js.js
@@ -0,0 +1,5 @@
+let nome = prompt("qual seu nome?")
+let interessse = prompt("qual seu interesse é fazer um site??? , contratar para sua empressa??? ou apenas conhecer???")
+alert(`prazer (${nome}) espero ajudar em seus  interesses em( ${interessse}) `)
+
+
