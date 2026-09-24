9a9c36275aefdf8060b401fb64e3b52ed8c77489
Matheus Henrique <matheus260524@gmail.com>
2026-09-24 07:51:45 -0300

    Delete PORTFOLIO directory

--- PORTFOLIO/PORTFOLIO/DESAFIO_HTML_CSS_TXT/js.js
+++
@@ -1,5 +0,0 @@
-let nome = prompt("qual seu nome?")
-let interessse = prompt("qual seu interesse é fazer um site??? , contratar para sua empressa??? ou apenas conhecer???")
-alert(`prazer (${nome}) espero ajudar em seus  interesses em( ${interessse}) `)
-
-
